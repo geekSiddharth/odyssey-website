bab1a09b10bf4463f6adb2abb7258ea48f96f1df
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-01-26 00:32:08 +0530

    Registeration Form, Event Description Page Added. pic in contac us centered. Event page header thinned

--- app.py
+++ app.py
@@ -12,5 +12,14 @@ def index():
 def events():
 	return render_template('events.html')
 
+
+# @app.route('/register')
+# def register():
+# 	return render_template('register.html')
+#
+# @app.route('/event-modal')
+# def event_modal():
+# 	return render_template('event-modal.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8000,debug=True)
