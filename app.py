9d0539c501c38c9a9c0a6f3e55b640ddc0df3ec0
Peeyush Kushwaha <peeyush.p97@gmail.com>
2017-01-26 13:08:49 +0530

    differentiate b/w EventID and Event Name, fix response type, change dialect to excel

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import os
 import csv
 import json
 
 """
 Structure of the csv file, event_data array:
+	Event ID: similar to event name, but without any spaces and spl characters except hyphen (-), and all lowercase
 	Event Name : 50 characters max
 	Description: 300 characters max
 	Team size: int
@@ -16,10 +17,10 @@ event_data = {}
 
 # read the file and save the data in mem when application is deployed.
 with open('event_data.csv', newline='') as file:
-	rows = csv.reader(file, delimiter=',')
+	rows = csv.reader(file, dialect='excel')
 	next(rows) # skips the first entry which has headings
 	for row in rows:
-		event_data[row[0]] = {"description": row[1], "team-size": int(row[2]), "rules": row[3], "contact": row[4]}
+		event_data[row[0]] = {"name": row[1], "description": row[2], "team-size": int(row[3]), "rules": row[4], "contact": row[5]}
 
 app = Flask('__name__')
 app.config['SECRET_KEY']=os.urandom(20)
@@ -34,7 +35,8 @@ def events_main():
 
 @app.route('/events/<event_name>')
 def event_particular(event_name):
-	return json.dumps(event_data[event_name])
+	data = json.dumps(event_data[event_name])
+	return Response(data, mimetype='application/json')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8000,debug=True)
