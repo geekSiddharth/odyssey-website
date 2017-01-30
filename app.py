3d227aa806738a42dec35d588bd6b6bca7cd666b
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-01-30 14:49:55 +0530

    event modal image added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, abort
-import os
 import csv
+import os
+
+from flask import Flask, render_template, abort
 
 """
 Structure of the csv file, event_data array:
@@ -18,22 +19,26 @@ event_data = {}
 
 # read the file and save the data in mem when application is deployed.
 with open('event_data.csv', newline='', encoding='utf8') as file:
-	rows = csv.reader(file, dialect='excel')
-	next(rows) # skips the first entry which has headings
-	for row in rows:
-		event_data[row[0]] = {"name": row[1], "description": row[2], "teamSize": row[3], "rules": row[4], "contact": row[5], "entryFee": row[6], "fbEventLink": row[7]}
-	file.close()
+    rows = csv.reader(file, dialect='excel')
+    next(rows)  # skips the first entry which has headings
+    for row in rows:
+        event_data[row[0]] = {"name": row[1], "description": row[2], "teamSize": row[3], "rules": row[4],
+                              "contact": row[5], "entryFee": row[6], "fbEventLink": row[7]}
+    file.close()
 
 app = Flask('__name__')
-app.config['SECRET_KEY']=os.urandom(20)
+app.config['SECRET_KEY'] = os.urandom(20)
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/events')
 def events():
-	return render_template('events.html')
+    return render_template('events.html')
+
 
 # @app.route('/register')
 # def register():
@@ -41,11 +46,12 @@ def events():
 
 @app.route('/events/<event_name>')
 def event_particular(event_name):
-	if event_name in event_data:
-		data = event_data[event_name]
-		return render_template('event-modal.html', event = data)
-	else:
-		abort(404)
+    if event_name in event_data:
+        data = event_data[event_name]
+        return render_template('event-modal.html', event=data)
+    else:
+        abort(404)
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=8000,debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
