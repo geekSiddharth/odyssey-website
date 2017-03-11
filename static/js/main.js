5b0881d4649ba6a925f0b90690c9cc1594d9a74d
Peeyush Kushwaha <peeyush.p97@gmail.com>
2017-03-11 18:08:26 +0530

    partial commit gurek pls save me

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-	setTimeout(function() {
-	$(".loader").fadeOut(2000); 
-	}, 4000);
+  $("#train-stop").click(function() {
+    $(".loader").fadeOut(2000); 
+  });
 });
-
-//var scene = document.getElementById('scene');
-//var parallax = new Parallax(scene);
