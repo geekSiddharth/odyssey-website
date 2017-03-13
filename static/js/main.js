e3a438d3287160b2d3c9721927e8e46f01288502
Peeyush Kushwaha <peeyush.p97@gmail.com>
2017-03-13 16:13:06 +0530

    more polishing

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   $("#train-stop").click(function() {
     $(".loader").fadeOut(2000); 
   });
+
+  $(".loader img").click(function (e) {
+    e.stopPropogation();
+  })
+  
+  $(".loader").click(function () {
+    $(".loader").fadeOut(2000); 
+  })
 });
